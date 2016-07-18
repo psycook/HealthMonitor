3aa7efb16fa115fc80a882f0e457198c86a84fc1
psycook <simon.m.cook@gmail.com>
2016-07-18 22:20:38 +0100

    first commit

---
+++ HealthMonitor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HealthMonitor
+//
+//  Created by Simon Cook on 18/07/2016.
+//  Copyright © 2016 Simon Cook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
