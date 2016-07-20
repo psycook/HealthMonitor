388460bb5cfecc8807d9fa97bb700f381cd14352
psycook <simon.m.cook@gmail.com>
2016-07-20 08:37:10 +0100

    health queries added

--- HealthMonitor WatchKit Extension/InterfaceController.swift
+++ HealthMonitor WatchKit Extension/InterfaceController.swift
@@ -11,7 +11,7 @@ import Foundation
 import HealthKit
 
 class InterfaceController: WKInterfaceController {
-
+    
     @IBOutlet var heartRate: WKInterfaceLabel!
     @IBOutlet var stepCount: WKInterfaceLabel!
     @IBOutlet var startStopButton: WKInterfaceButton!
@@ -24,12 +24,11 @@ class InterfaceController: WKInterfaceController {
         super.awakeWithContext(context)
         
         self.heartRate.setText("---")
-        self.heartRate.setTextColor(UIColor(red: 255, green:0,
-            blue: 0, alpha: 1))
+        self.heartRate.setTextColor(UIColor(red: 255, green:0, blue: 0, alpha: 1))
         self.stepCount.setText("---")
         self.stepCount.setTextColor(UIColor.greenColor())
     }
-
+    
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
@@ -65,12 +64,119 @@ class InterfaceController: WKInterfaceController {
                                         preferredStyle: .ActionSheet, actions: [buttonAction])
         return
     }
-
+    
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
-
+    
+    // Create our Heart Rate Query
+    func heartRateStreamingQuery(workoutStartDate: NSDate) -> HKQuery? {
+        guard let heartRateSample = HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierHeartRate) else { return nil
+        }
+        
+        let heartRateQuery = HKAnchoredObjectQuery(type:heartRateSample, predicate: nil, anchor: anchor, limit:
+        Int(HKObjectQueryNoLimit)) { (query, sampleObjects,
+            deletedObjects, newAnchor, error) -> Void in
+            guard let newAnchor = newAnchor else {return}
+            self.anchor = newAnchor
+        }
+        
+        heartRateQuery.updateHandler = {(query, samples, deleteObjects, newAnchor, error) -> Void in
+            self.anchor = newAnchor!
+            guard let heartRateSamples = samples as? [HKQuantitySample] else {
+                return
+            }
+            
+            dispatch_async(dispatch_get_main_queue()) {
+                guard let sample = heartRateSamples.first else {
+                    return
+                }
+                let value = sample.quantity.doubleValueForUnit(HKUnit(fromString:"count/min"))
+                self.heartRate.setText(String(UInt16(value)))
+            }
+            self.heartRate.setTextColor(UIColor(red: 255, green:0, blue: 0, alpha: 1))
+        }
+        return heartRateQuery
+    }
+    
+    // Query the HealthKit store to return the total number of
+    // steps attempted for a given day
+    func dailyStepsStreamingQuery(workoutStartDate: NSDate) -> HKQuery?
+    {
+        // Call our Sample type for our HealthKit class to return
+        guard let dailyStepsSample = HKSampleType.quantityTypeForIdentifier(HKQuantityTypeIdentifierStepCount) else {
+            return nil
+        }
+        let calendar = NSCalendar.currentCalendar()
+        let startDate = calendar.dateByAddingUnit(NSCalendarUnit.Day,value: -1, toDate: workoutStartDate, options:NSCalendarOptions.WrapComponents)
+        
+        let predicate = HKQuery.predicateForSamplesWithStartDate(startDate, endDate: workoutStartDate, options: HKQueryOptions.StrictEndDate)
+        let dailyStepsQuery = HKSampleQuery(sampleType: dailyStepsSample,
+                                            predicate: predicate, limit: 0, sortDescriptors: nil) {
+                                                [unowned self] (query, results, error) in
+                                                guard let dailyStepsSamples = results as?
+                                                    [HKQuantitySample] else {
+                                                        return
+                                                }
+                                                guard dailyStepsSamples.count > 0 else {
+                                                    return
+                                                }
+                                                
+                                                dispatch_async(dispatch_get_main_queue()) {
+                                                    var dailySteps : Double = 0
+                                                    for steps in dailyStepsSamples {
+                                                        dailySteps +=
+                                                            steps.quantity.doubleValueForUnit(HKUnit.countUnit())
+                                                    }
+                                                    self.stepCount.setText(String(UInt16(dailySteps)))
+                                                    self.stepCount.setTextColor(UIColor.greenColor())
+                                                }
+        }
+        
+        // Return our Query string back to the calling method
+        return dailyStepsQuery
+    }
+    
+    // Start a workout
+    func didSessionStart(date : NSDate) {
+        
+        // Start a Step Counter WorkOut
+        if let dailyStepsQuery = dailyStepsStreamingQuery(date) {
+            healthKitStore.executeQuery(dailyStepsQuery)
+        }
+        
+        // Start a Heart Rate WorkOut
+        if let heartRateQuery = heartRateStreamingQuery(date) {
+            healthKitStore.executeQuery(heartRateQuery)
+        }
+    }
+    
+    func didSessionEnd(date : NSDate) {
+        
+        // Stop Heart Rate Query from Running
+        if let heartRateQuery = heartRateStreamingQuery(date) {
+            healthKitStore.stopQuery(heartRateQuery)
+            self.heartRate.setText("---")
+        }
+        if let dailyStepsQuery = dailyStepsStreamingQuery(date) {
+            healthKitStore.stopQuery(dailyStepsQuery)
+            self.stepCount.setText("---")
+        }
+    }
+    
     @IBAction func startStopButtonPressed() {
+        if (self.isSessionActive)
+        {
+            // Finish the current session
+            self.isSessionActive = false
+            self.startStopButton.setTitle("Start Monitoring")
+            self.didSessionEnd(NSDate())
+        } else {
+            // Start a new session
+            self.isSessionActive = true
+            self.startStopButton.setTitle("Stop Monitoring")
+            self.didSessionStart(NSDate())
+        }
     }
 }
